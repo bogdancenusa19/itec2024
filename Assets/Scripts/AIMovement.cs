41d9434e3339d701186312833a3fc5796cda7504
AndreiSiu <vasiu_andrei2000@yahoo.com>
2024-04-06 13:15:23 +0300

    Add First Enemy

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [Header("Attributes")]
     [SerializeField] private float speed;
     [SerializeField] private float chaseDistance;
     [SerializeField] private float rangeDistance;
+
+    private Animator animator;
+    private PlayerMovement player;
     private float distance;
 
     private bool isPlayerInRange = true;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
@@ -18,6 +28,7 @@ public class AIMovement : MonoBehaviour
         {
             if (distance >= chaseDistance)
             {
+                animator.SetBool("isRunning", true);
                 transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
                 isPlayerInRange = true;
             }
@@ -25,6 +36,7 @@ public class AIMovement : MonoBehaviour
         else if (distance > chaseDistance)
         {
             isPlayerInRange = false;
+            animator.SetBool("isRunning", false);
         }
     }
 
