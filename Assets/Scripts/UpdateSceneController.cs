9f38d3608c5089b639a357c0a427191642a440e0
AndreiSiu <vasiu_andrei2000@yahoo.com>
2024-04-07 00:51:11 +0300

    Level 2 Shifting Scene

---
+++ Assets/Scripts/UpdateSceneController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSceneController : MonoBehaviour
+{
+    public GameObject background;
+    public Color newBackgroundColor;
+
+    public GameObject[] platforms;
+    public Color newPlatformColor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            background.GetComponent<SpriteRenderer>().color = newBackgroundColor;
+
+            foreach(GameObject platform in platforms)
+            {
+                platform.GetComponent<SpriteRenderer>().color = newPlatformColor;
+            }
+        }
+    }
+
+}
