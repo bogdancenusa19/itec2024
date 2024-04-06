e10032e252762b31b1d844f7ad4e2f4cf49dfa4d
bogdancenusa19 <bogdancenusa19@gmail.com>
2024-04-07 00:32:43 +0300

    Added inventory and eating animation

--- Assets/Scripts/UpdatePlayer.cs
+++ Assets/Scripts/UpdatePlayer.cs
@@ -9,6 +9,7 @@ public class UpdatePlayer : MonoBehaviour
     [SerializeField] private GameObject pistol;
     [SerializeField] private GameObject shotgun;
     [SerializeField] private GameObject drone;
+    [SerializeField] private GameObject lizard;
 
     [SerializeField] private Image pistolSprite;
     [SerializeField] private Image shotgunSprite;
@@ -17,17 +18,25 @@ public class UpdatePlayer : MonoBehaviour
     private bool hasPistol = false;
     private bool hasShotgun = false;
     private bool hasDrone = false;
+    private bool hasLizard = false;
 
     private bool isUsingPistol = true;
+
+    private int lizardsNumber = 5;
+
+    private Animator _animator;
+    
     private void Awake()
     {
         hasPistol = PlayerPrefs.GetInt("hasPistol") == 1;
         hasShotgun = PlayerPrefs.GetInt("hasShotgun") == 1;
         hasDrone = PlayerPrefs.GetInt("hasDrone") == 1;
+        hasLizard = PlayerPrefs.GetInt("hasLizard") == 1;
     }
 
     private void Start()
     {
+        _animator = gameObject.GetComponent<Animator>();
         if (hasPistol)
         {
             pistol.SetActive(true);
@@ -65,8 +74,24 @@ public class UpdatePlayer : MonoBehaviour
         }
     }
 
+    private void HideLizard()
+    {
+        lizard.SetActive(false);
+    }
+
+    private void EatLizard()
+    {
+        if (hasLizard && lizardsNumber > 0 && Input.GetKeyDown(KeyCode.Q))
+        {
+            lizard.SetActive(true);
+            _animator.SetTrigger("Eat");
+            lizardsNumber--;
+        }
+    }
+
     private void Update()
     {
         ChangeToShotgun();
+        EatLizard();
     }
 }
